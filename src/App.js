c54c3e5d08d7c2a216db5317915d04ea5f2db7fd
YuliaDenysiuk <yulia.denysiuk@gmail.com>
2021-10-01 01:54:28 +0300

    Task 1 - User Profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import user from "./user.json";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-  return <div className="App">Components</div>;
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 }
 
 export default App;
